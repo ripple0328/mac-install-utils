2e2645d43db4477fdb5915e0a0eff838789f2a19
Qingbo <ripple0328@gmail.com>
2014-01-04 16:11:39 +0800

    seperate package install status check and installation

--- install-utils.sh
+++ install-utils.sh
@@ -22,6 +22,22 @@ function colors () {
 }
 colors
 
+CURRENT_SHELL=$(echo $0 | tr -d -)
+SHELL_CONFIG_FILE=$(echo '.'$CURRENT_SHELL'rc')
+
+function setting-cask-install-path {
+    # for brew-cask
+    echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> $SHELL_CONFIG_FILE
+}
+
+function setting-path {
+    # for rvm
+    echo 'export PATH="/usr/local/bin:$PATH"' >> $SHELL_CONFIG_FILE
+}
+
+setting-path
+setting-cask-install-path
+
 function msg {
     printf "$BGREEN 0000===--->$RESET $2 $1 $RESET\n"
 }
@@ -33,45 +49,67 @@ function check-command-existence {
     hash $1 2>&-
 }
 
-function check-and-brew-install {
-    msg 'CHECKING\t\t whether -'$1'- has been installed' $BCYAN
+function is_brew_installed {
+    msg 'CHECKING\t\t whether -'$1'- has been installed' $BCYAN    
     brew list | grep $1  > /dev/null 2>&- &&
-    msg 'PACKAGE\t\t -'$1'- already been installed' $BYELLOW ||
-    ( msg 'PACKAGE\t\t -'$1'- not installed' $BRED
+    msg 'PACKAGE\t\t -'$1'- already been installed' $BYELLOW 
+}
+
+function install_brew_package {
     msg 'INSTALL\t\t -'$1'- by brew' $BPURPLE
-    
     brew install $1 $2 
     msg 'LINKING\t\t -'$1'- to system path' $BPURPLE
-    brew link $1 --force > /dev/null 2>&-)
+    brew link $1 --force > /dev/null 2>&-
     msg 'Upgrade\t\t -'$1'-' $BPURPLE
     brew upgrade $1 > /dev/null 2>&-
 }
 
-function check-and-npm-install {
+function check-and-brew-install {
+    is_brew_installed $1 ||
+    msg 'PACKAGE\t\t -'$1'- not installed' $BRED &&
+    install_brew_package $1 $2
+}
+
+function is-npm-packages-installed {
     msg 'CHECKING\t\t whether -'$1'- has been installed' $BCYAN    
     npm list -g --parseable | grep $1 > /dev/null 2>&-  &&
-    msg 'PACKAGE\t\t -'$1'- already been installed' $BYELLOW ||
-    (msg 'PACKAGE\t\t -'$1'- not installed' $BRED
+    msg 'PACKAGE\t\t -'$1'- already been installed' $BYELLOW 
+}
+
+function install-npm-package {
     msg 'INSTALL\t\t -'$1'- by npm' $BPURPLE
     npm install -g $1 > /dev/null 2>&-  
-    )
+}
+
+function check-and-npm-install {
+    is-npm-packages-installed $1 ||
+    msg 'PACKAGE\t\t -'$1'- not installed' $BRED &&
+    install-npm-package $1
+}
+
+function is-brew-repo-tapped {
+    msg 'CHECKING\t\t whether repo -'$1/$2'- has been taped' $BCYAN       
+    brew tap | grep $1/$2  >/dev/null 2>&- &&
+    msg 'REPO\t\t -- already been tapped' $BYELLOW 
 }
 
 function check-and-install-brew-repo {
-    msg 'CHECKING\t\t whether repo -'$1/$2'- has been taped' $BCYAN       
-    brew tap | grep $1/$2  >/dev/null 2>&- &&
-    msg 'REPO\t\t -- already been tapped' $BYELLOW ||
+    is-brew-repo-tapped $1 $2 ||   
     msg 'TAPPING\t\t -'$1/$2'- to brew'
     brew tap $1/homebrew-$2 >/dev/null 2>&-
 }
 
-function check-and-cask-install {
+function is-cask-package-installed {
     msg 'CHECKING\t\t whether package -'$1'- is installed' $BCYAN    
     brew cask list | grep $1  >/dev/null 2>&- &&
-    msg 'SOFTWARE\t\t -'$1'- already installed' $BYELLOW ||
-    (msg 'SOFTWARE\t\t -'$1'- not install by cask' $BRED
-    export HOMEBREW_CASK_OPTS="--appdir=/Applications"    
-    brew cask install $1 > /dev/null 2>&-)
+    msg 'SOFTWARE\t\t -'$1'- already installed' $BYELLOW
+}
+
+
+function check-and-cask-install {
+    is-cask-package-installed $1 ||
+    msg 'SOFTWARE\t\t -'$1'- not install by cask' $BRED &&
+    brew cask install $1 > /dev/null 2>&-
 }
 
 function install-tmate {
@@ -83,16 +121,26 @@ function cask-packages-path(){
     brew cask info $1 | sed -n 3p | awk '{split($0,a," "); print a[1]}'
 }
 
-function check-and-gem-install {
-    msg 'CHECKING\t\t whether gem -'$1'- is installed' $BCYAN
-    gem list | grep $1  > /dev/null 2>&- && 
-    msg 'GEM\t\t -'$1'- already been installed' $BYELLOW ||
-    ( msg 'GEM\t\t -'$1'- not been installed' $BRED
+function is_rvm_ruby {
+  msg 'CHECKING\t\t whether rvm ruby is installed' $BCYAN        
+  which ruby | grep rvm
+}
+
+function is_gem_installed {
+    gem list | grep $1  > /dev/null 2>&-  && 
+    msg 'GEM\t\t -'$1'- already been installed' $BYELLOW
+}
+function install_gem {
+    msg 'GEM\t\t -'$1'- not been installed' $BRED
     msg 'INSTALL\t\t gem -'$1'- for you' $BPURPLE
-    which ruby | grep rvm &&
+    is_rvm_ruby &&
     gem install -f $1 > /dev/null 2>&- ||
     sudo gem install -f $1 > /dev/null 2>&- 
-    )
+}
+
+function check-and-gem-install {
+    msg 'CHECKING\t\t whether gem -'$1'- is installed' $BCYAN
+    is_gem_installed $1  || install_gem $1
 }
 
 function cleanup-command {
@@ -106,7 +154,6 @@ function install-brew {
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
     msg 'UPDATING\t\t -homebrew-...' $BPURPLE
     brew update > /dev/null 2>&-
-    export PATH=/usr/local/bin:$PATH
 }
 
 function install-rvm {
@@ -118,17 +165,23 @@ function install-rvm {
     source /Users/`whoami`/.rvm/scripts/rvm
 }
 
-
+function alfred-index-brew-cask {
+    brew cask alfred link > /dev/null 2>&-
+}
 function install-brew-cask {
     check-and-install-brew-repo phinze cask
     check-and-brew-install brew-cask
-    brew cask alfred link > /dev/null 2>&-
+    alfred-index-brew-cask    
+}
+
+function setting-git-hub-alias {
+    echo 'alias git=hub' >> $SHELL_CONFIG_FILE
 }
 
 function install-git {
     check-and-brew-install git
     check-and-brew-install hub
-    alias git=hub
+    setting-git-hub-alias
 }
 function patch-shebang-path() {
     sed -i.bak '1 c\
@@ -145,9 +198,9 @@ function install-Inconsolata-powerline-font {
 }
 
 function get-root-permisson {
+   #TODO somettime the permission will expired
    sudo ls > /dev/null 2>&-
 }
-
 
 function  install_pow {
     curl get.pow.cx | sh    

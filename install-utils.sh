71b0e65db34fe2c2425f3b04db77cd81f1a354e0
Qingbo <ripple0328@gmail.com>
2014-05-04 16:55:31 +0800

    save .bashrc to ~

--- install-utils.sh
+++ install-utils.sh
@@ -234,7 +234,7 @@ function install-utils {
     cd ~
     curl -so.install-utils https://raw.github.com/ripple0328/mac-install-utils/master/install-utils.sh
     cat ~/$SHELL_CONFIG_FILE | grep install-utils  > /dev/null 2>&1 ||
-    (echo 'source ~/.install-utils' >> ./$SHELL_CONFIG_FILE
+    (echo 'source ~/.install-utils' >> ~/$SHELL_CONFIG_FILE
     source ~/$SHELL_CONFIG_FILE)
     source ~/.install-utils
 }
